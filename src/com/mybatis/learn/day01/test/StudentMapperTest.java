b4e266e657a83f31e38288512ec022e4c4860722
InfiniteAngel <1067750999@qq.com>
2019-05-22 17:27:15 +0800

    dayo2

--- src/com/mybatis/learn/day01/test/StudentMapperTest.java
+++ src/com/mybatis/learn/day01/test/StudentMapperTest.java
@@ -1,6 +1,7 @@
 package com.mybatis.learn.day01.test;
 
 import com.mybatis.learn.day01.mappers.StudentMapper;
+import com.mybatis.learn.day01.pojo.PhoneNumber;
 import com.mybatis.learn.day01.pojo.Student;
 import com.mybatis.learn.day01.utils.MybatisSqlSessionFactory;
 import org.apache.ibatis.io.Resources;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
+import java.util.List;
 
 public class StudentMapperTest {
     @Test
@@ -34,8 +36,51 @@ public class StudentMapperTest {
     @Test
     public void inserStudentTest2(){
         SqlSession sqlSession = MybatisSqlSessionFactory.openSession(true);
-        Student student = new Student(3,"XiaNa","XiaNa@qq.com",new Date());
+        PhoneNumber phone = new PhoneNumber("110","112","119");
+        Student student = new Student(4,"yan","yan@qq.com",new Date(),phone);
         StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
         mapper.insertStudent(student);
     }
+
+    @Test
+    public void findStudentById(){
+        SqlSession sqlSession = MybatisSqlSessionFactory.openSession();
+        StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
+        Student student = mapper.findStudentById(4);
+        System.out.println(student);
+    };
+
+    @Test
+    public void findAllStudentsTest(){
+        SqlSession sqlSession = MybatisSqlSessionFactory.openSession();
+        StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
+        List<Student> list = mapper.findAllStudents();
+        for (Student stu:list){
+            System.out.println(stu);
+        }
+    }
+
+    @Test
+    public  void updateStudentById(){
+        SqlSession sqlSession = MybatisSqlSessionFactory.openSession();
+        StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
+        mapper.updateStudentById(new Student(3,"yan.angel",null,null));
+        sqlSession.commit();
+    }
+    @Test
+    public void deleteStudentById(){
+        SqlSession sqlSession = MybatisSqlSessionFactory.openSession();
+        StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
+        mapper.deleteStudentById(3);
+        sqlSession.commit();
+    }
+    @Test
+    public void deleteAllStudents(){
+        SqlSession sqlSession = MybatisSqlSessionFactory.openSession();
+        StudentMapper mapper =sqlSession.getMapper(StudentMapper.class);
+        mapper.deleteAllStudents();
+        sqlSession.commit();
+    }
+
+
 }
